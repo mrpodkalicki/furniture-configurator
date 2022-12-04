338180a13dce5834b4faccd4ce00d1c65116d84c
Adrian Podkalicki <mr.podkalicki@gmail.com>
2022-12-04 16:46:01 +0100

    feat(Dashboard): add upload file functionality

--- src/app/dashboard/container/dashboard/dashboard.component.ts
+++ src/app/dashboard/container/dashboard/dashboard.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfiguratorFormService } from '@app/dashboard/shered/configurator-form/configurator-form.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidatorsService } from '@shared//form-builder/shared/custom-validators/custom-validators.service';
+import { FileReaderService } from '@shared//file-reader/shared/file-reader.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  constructor() {}
+export class DashboardComponent implements OnInit {
+  configuratorForm = this.createConfiguratorForm();
+
+  constructor(
+    private configuratorFormService: ConfiguratorFormService,
+    private customValidatorsService: CustomValidatorsService,
+    private fileReaderService: FileReaderService
+  ) {}
+
+  ngOnInit(): void {
+    console.log(this.configuratorForm);
+  }
+
+  getDataFromFile() {
+    const file = this.configuratorForm.value.file;
+    const fileData$ = this.fileReaderService.getDataFromFile(file);
+
+    fileData$.subscribe((data: any) => {
+      console.log(data, 'data');
+    });
+  }
+
+  private createConfiguratorForm(): FormGroup {
+    return this.configuratorFormService.createForm({
+      file: new FormControl(null, [
+        Validators.required,
+        this.customValidatorsService.requiredFileType('json'),
+        this.customValidatorsService.requiredFileType('xml'),
+      ]),
+    });
+  }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './container/dashboard/dashboard.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FileReaderModule } from '@shared//file-reader/file-reader.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FileReaderModule,
+    MatFormFieldModule,
+  ],
 })
 export class DashboardModule {}
